03d8a2f156cf7495b7a923ce7ee115fbe1fab370
Wyatt Evans <syattevans@gmail.com>
2020-07-24 10:15:27 -0600

    Removed comments from LoggedInUserView.

--- app/src/main/java/com/app/loginactivityjava/ui/login/LoggedInUserView.java
+++ app/src/main/java/com/app/loginactivityjava/ui/login/LoggedInUserView.java
@@ -1,11 +1,7 @@
 package com.app.loginactivityjava.ui.login;
 
-/**
- * Class exposing authenticated user details to the UI.
- */
 class LoggedInUserView {
     private String displayName;
-    //... other data fields that may be accessible to the UI
 
     LoggedInUserView(String displayName) {
         this.displayName = displayName;
