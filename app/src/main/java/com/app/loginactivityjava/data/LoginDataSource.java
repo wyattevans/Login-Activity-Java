05946cbecccba1b3e676ed7e31238a6626603f88
Wyatt Evans <syattevans@gmail.com>
2020-07-25 11:58:54 -0600

    Removed comments from LoginDataSource.

--- app/src/main/java/com/app/loginactivityjava/data/LoginDataSource.java
+++ app/src/main/java/com/app/loginactivityjava/data/LoginDataSource.java
@@ -4,15 +4,11 @@ import com.app.loginactivityjava.data.model.LoggedInUser;
 
 import java.io.IOException;
 
-/**
- * Class that handles authentication w/ login credentials and retrieves user information.
- */
 public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
 
         try {
-            // TODO: handle loggedInUser authentication
             LoggedInUser fakeUser =
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
@@ -24,6 +20,6 @@ public class LoginDataSource {
     }
 
     public void logout() {
-        // TODO: revoke authentication
+
     }
 }
