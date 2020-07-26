4e5fcc0b37c8d5efaf6966a9effc429b708a56d4
Wyatt Evans <syattevans@gmail.com>
2020-07-26 00:01:48 -0600

    Removed logs and updated gradle.

--- app/src/main/java/com/app/loginactivityjava/ui/login/LoginActivity.java
+++ app/src/main/java/com/app/loginactivityjava/ui/login/LoginActivity.java
@@ -1,16 +1,7 @@
 package com.app.loginactivityjava.ui.login;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
@@ -22,9 +13,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import com.app.loginactivityjava.R;
-import com.app.loginactivityjava.ui.login.LoginViewModel;
-import com.app.loginactivityjava.ui.login.LoginViewModelFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
